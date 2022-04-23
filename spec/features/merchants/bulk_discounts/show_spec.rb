70d93afd64f536361a94e2662a64763696fb4de1
Drew Proebstel <d.proebstel@gmail.com>
2022-04-22 22:08:20 -0700

    feature spec: bulk discount show page

---
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'new merchant bulk discount' do
+  it 'creates a new bulk discount then routes to the index page' do
+    merchant_1 = Merchant.create(name: "Drew's")
+    merchant_2 = Merchant.create(name: "Geddy's")
+
+    bulk_discount_1 = merchant_1.bulk_discounts.create(percentage_discount: 20, quantity_threshold:15  )
+    bulk_discount_2 = merchant_1.bulk_discounts.create(percentage_discount: 30, quantity_threshold:25  )
+    bulk_discount_3 = merchant_2.bulk_discounts.create(percentage_discount: 10, quantity_threshold:22  )
+
+    expect(page).to have_current_path("/merchants/#{merchant_1.id}/bulk_discounts/#{bulk_discount_1.id}")
+    expect(page).to have_content('20%')
+    expect(page).to have_content('15')
+    expect(page).to_not have_content('10%')
+    expect(page).to_not have_content('22')
+  end
+end
