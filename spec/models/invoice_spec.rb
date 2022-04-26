1574f870431ae9cc7576fd23e45c882f0e941508
Drew Proebstel <d.proebstel@gmail.com>
2022-04-25 23:37:39 -0700

    feature:discount for one merchant

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -100,15 +100,15 @@ RSpec.describe Invoice, type: :model do
       bulk_discount_2 = walmart.bulk_discounts.create!(quantity_threshold: 100, percentage_discount: 35)
       bob = Customer.create!(first_name: "Bob", last_name: "Benson")
       item_1 = walmart.items.create!(name: "pickle", description: "sour cucumber", unit_price: 300)
-
+      item_2 = walmart.items.create!(name: "ray gun", description: "pew pew", unit_price: 2000)
       invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+      invoice_2 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
 
       InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 80, status: 1, unit_price: item_1.unit_price)
-      InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 110, status: 1, unit_price: item_1.unit_price)
+      InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_2.id, quantity: 110, status: 1, unit_price: item_2.unit_price)
 
-      expect(discount_for_merchant(walmart)).to eq(39450)
-
-  describe 'instance methods' do
+      expect(invoice_1.discount_for_merchant(walmart)).to eq(830.0)
+    end
     it '#invoice_total_revenue_after_discount' do
       walmart = Merchant.create!(name: "Wal-Mart")
 
@@ -116,11 +116,12 @@ RSpec.describe Invoice, type: :model do
       bulk_discount_2 = walmart.bulk_discounts.create!(quantity_threshold: 100, percentage_discount: 35)
       bob = Customer.create!(first_name: "Bob", last_name: "Benson")
       item_1 = walmart.items.create!(name: "pickle", description: "sour cucumber", unit_price: 300)
-
+      item_2 = walmart.items.create!(name: "ray gun", description: "pew pew", unit_price: 2000)
       invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+      invoice_2 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
 
       InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 80, status: 1, unit_price: item_1.unit_price)
-      InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 110, status: 1, unit_price: item_1.unit_price)
+      InvoiceItem.create!(invoice_id: invoice_2.id, item_id: item_2.id, quantity: 110, status: 1, unit_price: item_2.unit_price)
 
       expect(invoice_1.invoice_total_revenue_after_discount).to eq(12223)
     end
