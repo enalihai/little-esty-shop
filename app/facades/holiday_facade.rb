68e6d429f146d6a3d471ac7b9b3bec47e722a3e4
Drew Proebstel <d.proebstel@gmail.com>
2022-04-26 21:33:19 -0700

    feature:3 holidays on discount index

--- app/facades/holiday_facade.rb
+++ app/facades/holiday_facade.rb
@@ -7,7 +7,11 @@ class HolidayFacade
     @_holiday_data ||= service.get_holidays
   end
 
-  def holidays
-    Holiday.new(holiday_data)
+  def three_holidays
+    holidays = []
+    holiday_data[0..2].each do |holiday_sub_data|
+      holidays << Holiday.new(holiday_sub_data)
+    end
+    holidays
   end
 end
